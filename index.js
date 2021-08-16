fd624ffab6b746da37919b78f50fa2e3acf00b71
Mushtaq-A-Halepoto <mahalepoto@outlook.com>
2021-08-16 17:39:12 +0100

    js file added for new google map.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+      // Initialize and add the map
+      function initMap() {
+        // The location of Uluru
+        const uluru = { lat: -0.17631471383349637, lng: 51.61390244284751 };
+        // The map, centered at Uluru
+        const map = new google.maps.Map(document.getElementById("map"), 
+        {
+          zoom: 4,
+          center: uluru,
+        });
+        // The marker, positioned at Uluru
+        const marker = new google.maps.Marker({
+          position: uluru,
+          map: map,
+        });
+      }
